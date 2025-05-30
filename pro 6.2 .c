2884060ef9dc5a10346c9e424635d29cfea18c83
akbariprit <prit03566@gmail.com>
2025-05-30 17:22:58 +0530

    Add files via upload

---
+++ pro 6.2 .c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char s[100];
+    int l, b;
+    int a[100];
+
+    printf("Enter any string: ");
+    gets(s);
+
+    l=strlen(s);
+
+    printf("Frequency of each letter:\n");
+
+    for(int i=0; i<l; i++) {
+        if(a[i]==1)
+        continue;
+        b=1;
+        for(int j=i+1; j<l; j++) {
+            if (s[i]==s[j]) {
+            b++;
+            a[j] = 1;
+            }
+        }
+    printf("%c => %d\n", s[i], b);
+    }
+}
+
+
+
